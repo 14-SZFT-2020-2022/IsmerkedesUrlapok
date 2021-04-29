721373abc49159f8357324fc6af10cf2e8a86b9d
pepe1973 <bodrogipr@gmail.com>
2021-04-29 10:35:42 +0200

    Első kommit

---
+++ Ismerkedes/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ismerkedes
+{
+    public partial class Urlap : Form
+    {
+        public Urlap()
+        {
+            InitializeComponent();
+        }
+    }
+}
